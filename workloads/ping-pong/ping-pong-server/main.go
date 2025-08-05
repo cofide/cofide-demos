526d06455a3caf749b884bc2e5323cd4eec7d86d
Maartje Eyskens <maartje@eyskens.me>
2025-08-05 11:47:37 +0200

    Use prometheus for metrics on a seperate port.

--- workloads/ping-pong/ping-pong-server/main.go
+++ workloads/ping-pong/ping-pong-server/main.go
@@ -2,29 +2,60 @@ package main
 
 import (
 	"context"
-	"expvar"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
 	"github.com/spiffe/go-spiffe/v2/workloadapi"
 )
 
 // Metrics counters
 var (
-	successfulConnections = expvar.NewInt("successful_connections")
-	handlerErrors         = expvar.NewInt("handler_errors")
-	svidFailures          = expvar.NewInt("svid_failures")
-	tlsErrors             = expvar.NewInt("tls_errors")
-	requestsTotal         = expvar.NewInt("requests_total")
-	serverStartTime       = expvar.NewInt("server_start_time")
+	handlerErrors = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "handler_errors",
+		Help: "The total number of handler errors",
+	})
+	svidFailures = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "svid_failures",
+		Help: "The total number of SVID failures",
+	})
+	tlsErrors = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "tls_errors",
+		Help: "The total number of TLS errors",
+	})
+	requestsTotal = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "requests_total",
+		Help: "The total number of requests",
+	})
+	serverStartTime = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "server_start_time",
+		Help: "The timestamp when the server started",
+	})
+
+	successfulConnections = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "successful_connections",
+		Help: "The total number of successful connections",
+	})
+
+	lastX509SourceUpdate = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "last_x509_source_update",
+		Help: "The timestamp of the last X509Source update",
+	})
+
+	svidNotAfter = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "svid_not_after",
+		Help: "The timestamp when the current SVID certificate expires (NotAfter)",
+	})
 )
 
 func main() {
-	serverStartTime.Set(time.Now().Unix())
+	serverStartTime.Set(float64(time.Now().Unix()))
 
 	if err := run(context.Background(), getEnv()); err != nil {
 		log.Fatal(err)
@@ -33,6 +64,7 @@ func main() {
 
 type Env struct {
 	Port             string
+	MetricsPort      string
 	SpiffeSocketPath string
 	MetricsEnabled   bool
 }
@@ -48,6 +80,7 @@ func getEnvWithDefault(variable string, defaultValue string) string {
 func getEnv() *Env {
 	return &Env{
 		Port:             getEnvWithDefault("PORT", ":8443"),
+		MetricsPort:      getEnvWithDefault("METRICS_PORT", ":8080"),
 		SpiffeSocketPath: getEnvWithDefault("SPIFFE_ENDPOINT_SOCKET", "unix:///spiffe-workload-api/spire-agent.sock"),
 	}
 }
@@ -58,8 +91,10 @@ func run(ctx context.Context, env *Env) error {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", metricsWrapper(handler))
+
 	// Expose metrics endpoint
-	mux.Handle("/debug/vars", expvar.Handler())
+	http.Handle("/metrics", promhttp.Handler())
+	go http.ListenAndServe(env.MetricsPort, nil)
 
 	source, err := workloadapi.NewX509Source(ctx,
 		workloadapi.WithClientOptions(
@@ -67,10 +102,38 @@ func run(ctx context.Context, env *Env) error {
 		),
 	)
 	if err != nil {
-		svidFailures.Add(1)
+		svidFailures.Inc()
 		return fmt.Errorf("unable to obtain SVID: %w", err)
 	}
 	defer source.Close()
+
+	// Monitor SVID updates
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-source.Updated():
+				lastX509SourceUpdate.Set(float64(time.Now().Unix()))
+
+				svid, err := source.GetX509SVID()
+				if err != nil {
+					log.Printf("Error getting X509SVID: %v", err)
+					continue
+				}
+				if len(svid.Certificates) > 0 {
+					notAfter := svid.Certificates[0].NotAfter
+					svidNotAfter.Set(float64(notAfter.Unix()))
+				}
+			}
+		}
+	}()
+
+	// Set initial X509 info in metrics
+	lastX509SourceUpdate.Set(float64(time.Now().Unix()))
+	if svid, err := source.GetX509SVID(); err == nil && len(svid.Certificates) > 0 {
+		svidNotAfter.Set(float64(svid.Certificates[0].NotAfter.Unix()))
+	}
 
 	tlsConfig := tlsconfig.MTLSServerConfig(source, source, tlsconfig.AuthorizeAny())
 	server := &http.Server{
@@ -80,10 +143,10 @@ func run(ctx context.Context, env *Env) error {
 		ReadHeaderTimeout: time.Second * 10,
 	}
 
-	log.Printf("Server starting on %s with metrics at /debug/vars", env.Port)
+	log.Printf("Server starting on %s with metrics at /metrics on %s", env.Port, env.MetricsPort)
 
 	if err := server.ListenAndServeTLS("", ""); err != nil {
-		tlsErrors.Add(1)
+		tlsErrors.Inc()
 		return fmt.Errorf("failed to serve: %w", err)
 	}
 
@@ -92,8 +155,8 @@ func run(ctx context.Context, env *Env) error {
 
 func metricsWrapper(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		requestsTotal.Add(1)
-		successfulConnections.Add(1)
+		requestsTotal.Inc()
+		successfulConnections.Inc()
 		next(w, r)
 	}
 }
@@ -103,7 +166,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte("...pong"))
 	if err != nil {
-		handlerErrors.Add(1)
+		handlerErrors.Inc()
 		log.Printf("Error writing response: %v", err)
 		return
 	}
